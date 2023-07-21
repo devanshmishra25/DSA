b1b9729d13777625d6576862ba7cb8cb7b6fcf23
devanshmishra25 <80719667+devanshmishra25@users.noreply.github.com>
2023-07-22 01:00:05 +0530

    Time: 47 ms (6.36%), Space: 20.6 MB (5.97%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,14 +1,28 @@
+/*The steps are as follows:
+
+First, we will declare a map.
+
+Now, using a loop we will store the elements of the array along with their frequency in the map data structure.
+
+Using another loop we will iterate over the map, and try to find the element for which the frequency is 1, and finally, we will return that particular element.*/
+
 class Solution {
 public:
-    int singleNumber(vector<int>& nums) {
-
-        sort(nums.begin(), nums.end());
-        for(int i = 0; i<nums.size() - 1; i+=2){
-            if(nums[i] != nums[i+1]){
-                return nums[i];
-            } 
+    int singleNumber(vector<int>& arr) {
         
-    }
-    return nums.back();
+        int n = arr.size();
+        map<int, int> mpp;
+        for(int  i = 0; i<n; i++){
+            mpp[arr[i]]++;
+            
+        }
+        
+        for(auto it: mpp){
+            if(it.second == 1)
+                return it.first;
+        }
+        
+        return -1;
+        
     }
 };
